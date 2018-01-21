a47a47a9145166f8e3c405f05ee2392d00f9c58a
Ben Harrington <harringtonben@icloud.com>
2018-01-21 10:53:59 -0600

    have planets separated into new lists

--- planetsandspaceships/Program.cs
+++ planetsandspaceships/Program.cs
@@ -13,16 +13,25 @@ namespace planetsandspaceships
             List<string> planetList = new List<string>() { "Mercury", "Mars" };
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
+            
 
-            List<string> lastPlanets = new List<string>() { "Uranus", "Neptune" };
+            List<string> lastPlanets = new List<string>() { "Uranus", "Neptune", "Pluto" };
 
             planetList.AddRange(lastPlanets);
             planetList.Insert(1, "Venus");
             planetList.Insert(2, "Earth");
 
+            List<string> rockyPlanets = planetList.GetRange(0, 4).ToList();
 
+            planetList.Remove("Pluto");
 
             foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
+            Console.WriteLine();
+            foreach (var planet in rockyPlanets)
             {
                 Console.WriteLine(planet);
             }
